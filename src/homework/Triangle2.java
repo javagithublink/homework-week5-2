297924cb03750f855cc111b90d65f999d859cb2f
yellowsunshine <sidnav999@gmail.com>
2021-11-14 18:36:39 +0000

    KD 1st change on intellij

--- src/homework/Triangle2.java
+++ src/homework/Triangle2.java
@@ -6,7 +6,7 @@ package homework;
 public class Triangle2 {
 
     public static void main(String[] args) {
-
+        System.out.println("Kinjal, the best!");
         System.out.println("     * ");
         System.out.println("    * *");
         System.out.println("   * * *");
