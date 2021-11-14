0a7e8d406a8ad3691a35b8a19bbae609f39bb52e
billi0 <sns.globalmkt@gmail.com>
2021-11-14 18:59:51 +0000

    this is unconflicted main

--- src/homework/Triangle2.java
+++ src/homework/Triangle2.java
@@ -6,7 +6,7 @@ package homework;
 public class Triangle2 {
 
     public static void main(String[] args) {
-
+        System.out.println("shakeel ");
         System.out.println("     * ");
         System.out.println("    * *");
         System.out.println("   * * *");
